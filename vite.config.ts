d54b82061c2f3c2e2b912be995a511fc2a4578fc
Mohamed Elshoraky <officialshoraky@gmail.com>
2025-02-13 02:20:38 +0200

    update paths, prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "gpa-calculator",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
